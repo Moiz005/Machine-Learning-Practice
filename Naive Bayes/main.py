3b983af590192f6942433bea49ce79487810821c
Muhammad Moiz <97216206+Moiz005@users.noreply.github.com>
2024-07-05 16:51:25 +0500

    Add files via upload

---
+++ Naive Bayes/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+df = pd.read_csv('Social_Network_Ads.csv')
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_pred, y_test)
+print(cm)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
